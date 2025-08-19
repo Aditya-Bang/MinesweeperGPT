d9456fe4a375b7e802bdf12330cc6f42fcf79e56
Aditya Bang <adityabang15@gmail.com>
2025-08-19 17:45:49 -0400

    tests for board done

---
+++ tests/board.py
@@ -0,0 +1,13 @@
+from app.board import Board
+
+b = Board()
+b.generate_random_board()
+b.print_board(reveal_hidden=True)   # Debug: see mines
+
+alive = b.reveal(0, 0)
+b.print_board()
+
+if not alive:
+    print("💥 You hit a mine!")
+elif b.check_win():
+    print("🎉 You won!")
