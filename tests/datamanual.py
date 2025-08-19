460f82a7054e78fec7c2190238803154e1be320c
Aditya Bang <adityabang15@gmail.com>
2025-08-19 19:15:45 -0400

    make new file for testing data creation from board solves

---
+++ tests/datamanual.py
@@ -0,0 +1,19 @@
+# tests/datamanual.py
+from app.validboard import ValidBoard
+from app.minesweepersolver import MinesweeperSolver
+
+
+vb = ValidBoard(rows=8, cols=8, mines=12)
+first_reveal = vb.reveal(4, 4)
+solver = MinesweeperSolver(vb.board)
+solver.solve()
+
+vb.print_board(reveal_hidden=False)
+vb.print_board(reveal_hidden=True)
+
+for r in range(vb.rows):
+    for c in range(vb.cols):
+        if vb.board.board[r][c] == 'F':
+            assert vb.board.hidden_board[r][c] == 'M', f"Flag at ({r},{c}) is not a mine!"
+
+print("All clear")
