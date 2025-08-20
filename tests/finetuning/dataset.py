a78a4f14fbc71e0177774c50cb13dde10b36a88c
Aditya Bang <adityabang15@gmail.com>
2025-08-20 18:13:05 -0400

    testing format example

--- tests/finetuning/dataset.py
+++ tests/finetuning/dataset.py
@@ -1,5 +1,10 @@
 # tests/finetuning/dataset.py
 from src.finetuning.dataset import MinesweeperDataset
+from src.models import MinesweeperExample
+from src.finetuning.prompt import format_example
+from typing import List
+
+
 ds = MinesweeperDataset("data")
-ex = ds.load_examples()[0]
-print(ex)
+ex: MinesweeperExample = ds.load_examples()[0]
+print(format_example(ex))
