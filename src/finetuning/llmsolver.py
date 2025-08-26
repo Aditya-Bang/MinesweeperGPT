eb1cba2fe03112345cbc69a6c8196d6fd19b2437
Aditya Bang <aditya.bang@nokia.com>
2025-08-26 13:28:22 -0400

    made structure for llm solver

--- src/finetuning/llmsolver.py
+++ src/finetuning/llmsolver.py
@@ -2,6 +2,7 @@
 from src.minesweeper.validboard import ValidBoard
 from src.models import Move
 from src.finetuning.prompt import format_example
+from typing import List, Dict
 
 
 class LLMSolver:
@@ -12,17 +13,37 @@ class LLMSolver:
 
     def solve(self, problem: str) -> str:
         # Implement the logic to solve the problem using the LLM
+        # specifically get's an llm move, makes it, and checks if game over on valid board
         return f"Solved {problem} using {self.model_name}"
-
-    def get_llm_move(self) -> str:
-        # Implement the logic to get a single move from the LLM
-        messages: dict = format_example(self.board.get_board_string())
-        return f"Get single move using {self.model_name}"
     
-    def parse_llm_move(self, llm_move: str) -> Move:
+    def get_llm_move(self) -> Move:
+        # generates, parses, and then validates llm move, i.e. it should be playable.
+        pass
+
+    def _generate_llm_move(self) -> str:
+        prompt: List[Dict[str, str]] = format_example(self.board.get_board_string())
+        text = self.tokenizer.apply_chat_template(
+            prompt,
+            tokenize=False,
+            add_generation_prompt=True,
+        )
+        input_tokens = self.tokenizer([text], return_tensors="pt").to(self.model.device)
+        generated_ids = self.model.generate(
+            **input_tokens,
+            max_new_tokens=512,
+        )
+        output_ids = generated_ids[0][len(input_tokens.input_ids[0]):].tolist()
+        decoded_output: str = self.tokenizer.decode(output_ids, skip_special_tokens=True).strip("\n")
+        return decoded_output
+    
+    def _parse_llm_move(self, llm_move: str) -> Move:
         # Implement the logic to parse the LLM move
         return Move(row=0, col=0, action="reveal")
 
-    def make_move(self, move: Move):
-        # Implement the logic to make a move on the board
+    def _validate_llm_move(self, move: Move):
+        # Implement the logic to validate the LLM move
+        pass
+
+    def make_llm_move(self):
+        # updates valid board with llm move by playing it.
         pass
